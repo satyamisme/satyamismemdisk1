43302c29a6ca9d645af358307e1f8e2c7fb22544
Satyamisme <satyamisme@gmail.com>
2022-11-21 16:08:53 +0300

    Update leechstart.sh

--- leechstart.sh
+++ leechstart.sh
@@ -8,8 +8,8 @@ echo yes | pip3 install -r requirements.txt
 echo yes | sudo docker container prune
 echo yes | sudo docker image prune -a
 sudo dockerd
-sudo docker build . -t mdisk
-sudo docker run -p 54:54 mdisk
+sudo docker build . -t mdisk1
+sudo docker run -p 56:56 mdisk1
 
 
 
